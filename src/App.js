bf05529acdd22e94629fe673b8dc0991e05d9f29
wlsu <wlsu@shoalter.com>
2022-09-01 21:51:23 +0800

    Clean code

--- src/App.js
+++ src/App.js
@@ -29,18 +29,4 @@ const App = () => {
   );
 };
 
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-        
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-       
-//       </header>
-//     </div>
-//   );
-// }
-
 export default App;
